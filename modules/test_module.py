6db95f57d87b9e718e9862dbb71dcdc5f3cdfffe
kssim <ksub0912@gmail.com>
2016-08-01 12:34:15 +0900

    Add daemon example.

--- modules/test_module.py
+++ modules/test_module.py
@@ -1,7 +1,11 @@
+import dbus
 from sys import path
 path.append('../')
 from ptysh_module import PtyshModule
 from ptysh_util import Singleton
+
+DBUS_BUS_NAME = 'com.kssim.test'
+DBUS_OBJECT_PATH = '/com/kssim/test'
 
 class test_module(PtyshModule, Singleton):
 
@@ -9,14 +13,18 @@ class test_module(PtyshModule, Singleton):
         PtyshModule.__init__(self)
         PtyshModule.set_node_name(self, 'hello')
         PtyshModule.set_command(self, 'hello', 'say hello', self.cmd_print_hello, False, True)
-        PtyshModule.set_command(self, 'world', 'say world', self.cmd_print_world, False, True)
         PtyshModule.set_command(self, 'hello_world', 'say hello world', self.cmd_print_hello_world, True, True)
+        PtyshModule.set_command(self, 'change_msg', 'change print msg', self.cmd_send_msg_to_daemon, False, True)
 
     def cmd_print_hello(self):
         print ('hello')
 
-    def cmd_print_world(self):
-        print ('world')
-
     def cmd_print_hello_world(self):
         print ('hello world')
+
+    def cmd_send_msg_to_daemon(self):
+        bus = dbus.SystemBus()
+        bus_object = bus.get_object(DBUS_BUS_NAME, DBUS_OBJECT_PATH)
+        bus_interface = dbus.Interface(bus_object, DBUS_BUS_NAME)
+
+        bus_interface.receive_signal('Send message is worked.')

8bd90fa53c5c4e88eeb169c5a4cc1b448fb2c1f1
kssim <kssim@pentasecurity.com>
2018-02-06 09:36:35 +0900

    Refactoring test module

--- modules/test_module.py
+++ modules/test_module.py
@@ -8,23 +8,29 @@ DBUS_OBJECT_PATH = "/com/kssim/test"
 
 class test_module(PtyshModule):
 
+    test_string = ""
+
     def __init__(self):
         super(test_module, self).__init__()
         super(test_module, self).init_node("hello", "hello node")
         super(test_module, self).set_dbus_info(DBUS_BUS_NAME, DBUS_OBJECT_PATH, DBUS_BUS_NAME)
-        super(test_module, self).add_command("hello", "say hello", self.cmd_print_hello)
-        super(test_module, self).add_command("hello_world", "say hello world", self.cmd_print_hello_world)
-        super(test_module, self).add_command("argument", "argument", self.cmd_print_argument, "Please enter one or more arguments")
+        super(test_module, self).add_command("show hello", "show hello", self.cmd_show_hello)
+        super(test_module, self).add_command("show world", "show hello world", self.cmd_show_hello_world)
+        super(test_module, self).add_command("argument", "argument", self.cmd_set_argument, "Please enter one or more arguments")
+        super(test_module, self).add_command("show argument", "show argument", self.cmd_show_argument)
         super(test_module, self).add_command("change_msg", "change print msg", super(test_module, self).dbus_handler, "", False)
 
-    def cmd_print_hello(self):
+    def cmd_show_hello(self):
         print ("hello")
 
-    def cmd_print_hello_world(self):
+    def cmd_show_hello_world(self):
         print ("hello world")
 
-    def cmd_print_argument(self, arg):
+    def cmd_set_argument(self, arg):
         if len(arg):
-            print (" ".join([str(x) for x in arg]))
+            self.test_string  = " ".join([str(x) for x in arg])
             return True
         return False
+
+    def cmd_show_argument(self):
+        print (self.test_string)
